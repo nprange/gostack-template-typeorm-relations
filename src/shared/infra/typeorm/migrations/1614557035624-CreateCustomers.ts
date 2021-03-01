7cfac1ad9a48ffa46db50269dd2e04baa4be6968
Nicolas Prange <nicolasprange@MacBook-Pro-de-Nicolas.local>
2021-03-01 04:27:25 -0300

    Adjust ormconfig

--- src/shared/infra/typeorm/migrations/1614557035624-CreateCustomers.ts
+++ src/shared/infra/typeorm/migrations/1614557035624-CreateCustomers.ts
@@ -3,7 +3,6 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 export default class CreateCustomers1614557035624
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
     await queryRunner.createTable(
       new Table({
         name: 'customers',
